e31547ee04f15e386cccccec0203b34b6ca7cadd
dprzygocka <przygocka.dagmara@gmail.com>
2022-10-24 12:59:35 +0200

    min cut

--- ff.py
+++ ff.py
@@ -12,11 +12,10 @@ for _ in range(int(input())):
         capacity = sys.maxsize
     graph[from_id][to_id] = capacity
     graph[to_id][from_id] = capacity
-print(graph)
 
+org_graph = [i[:] for i in graph]
 # Breadth-first search Network traversal
 def BFS(from_id, target, path):
-    # check if node is in Network
 
     # mark all vertices as not visited and create queue
     visited = [False] * number_of_nodes
@@ -39,7 +38,6 @@ def BFS(from_id, target, path):
 def ford_fulkerson(from_id, target):
     path = [-1] * number_of_nodes
     max_flows = 0
-    print(max_flows)
     while BFS(from_id, target, path):
         flow = sys.maxsize
         start_node = target
@@ -48,7 +46,6 @@ def ford_fulkerson(from_id, target):
             flow = min(flow, graph[path[start_node]][start_node]) 
             start_node = path[start_node]
         max_flows = max_flows + flow
-        print(flow)
 
         #update residual graph
         start_node2 = target
@@ -57,6 +54,40 @@ def ford_fulkerson(from_id, target):
             graph[u][start_node2]= graph[u][start_node2] - flow
             graph[start_node2][u] = graph[start_node2][u] + flow
             start_node2 = u
+
     return max_flows
 
-print(ford_fulkerson(0,54))
+# Function for Depth first search
+# Traversal of the graph
+def dfs(graph, s, visited):
+    visited[s] = True
+    for v in range(len(graph)):
+        # If there is an edge and we haven't visited
+        if graph[s][v] > 0 and not visited[v]:
+            # Call recusively from new vertex
+            dfs(graph, v, visited)
+
+def min_cut(source):
+    if not org_graph:
+        return False
+
+    edges = []
+
+    # Perform DFS
+    visited = [False] * len(graph)
+    dfs(graph, source, visited)
+
+    # Check for edges that had a capacity in the original graph, but
+    # now has 0 capacity.
+    # Also has to go from visited in the DFS to not visited
+    for i in range(len(graph)):
+        for j in range(len(graph)):
+            if graph[i][j] == 0 and org_graph[i][j] > 0 and visited[i] and not visited[j]:
+                edges.append((i, j, org_graph[i][j]))
+
+    return edges
+
+
+print(f"Max flow: {ford_fulkerson(0,54)}")
+print(f"Min cut: {min_cut(0)}")
+
